482b657ab1078cb5b249004c767235647dfbb526
Vladimir Korotkov <kvamob@mail.ru>
2017-10-21 21:12:26 +0500

    Начал работу над скриптом загрузки карты Yandex

---
+++ load_map_image.py
@@ -0,0 +1,25 @@
+# Получение карты Yandex Maps по координатам центра
+# Описание API https://tech.yandex.ru/maps/doc/staticapi/1.x/dg/concepts/input_params-docpage/
+# Результат - в файле ./images/map.png
+# Используется библиотека requests (pip3 install requests)
+
+
+import shutil
+import requests
+
+out_filename = './images/map.png'
+lat = '56.747179'   # Широта
+lon = '60.67913'    # Долгота
+scale = 14          # Масштабирование
+
+# url = 'https://static-maps.yandex.ru/1.x/?pt={0},{1},comma&z={2}&size=600,450&l=map,skl'.format(lon, lat, scale)
+url = 'https://static-maps.yandex.ru/1.x/?pt={0},{1},comma&z={2}&size=600,450&l=map'.format(lon, lat, scale)
+
+response = requests.get(url, stream=True)
+if response.status_code == 200:
+    print('Запрос выполнен успешно')
+    with open(out_filename, 'wb') as out_file:
+        shutil.copyfileobj(response.raw, out_file)
+else:
+    print('Ошибка, status code: {}'.format(response.status_code))
+del response
