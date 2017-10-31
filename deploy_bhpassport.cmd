afaeae61f56ba3d5eb28dcb39a8a51d26fa777f1
Vladimir Korotkov <kva@delrus.ru>
2017-10-31 09:25:26 +0500

    Добавлен скрипт деплоя паспорта скважины

---
+++ deploy_bhpassport.cmd
@@ -0,0 +1,23 @@
+@echo off
+
+chcp 1251 > nul
+SET SRC_DIR="D:\GIT-REPOS\latex-templates\bhpassport"
+SET DST_DIR="D:\Home System\ИЗЫСКАНИЯ\Паспорта скважин\2017\_Паспорт_"
+chcp 866 > nul
+
+cls
+echo ################################################################################################
+
+mkdir %DST_DIR%
+mkdir %DST_DIR%\images
+
+copy %SRC_DIR%\*.tex %DST_DIR% > nul
+copy %SRC_DIR%\images\* %DST_DIR%\images > nul
+
+echo Source directory : %SRC_DIR%
+echo Target directory : %DST_DIR%
+
+echo ################################################################################################
+
+rem echo "Done..."
+
