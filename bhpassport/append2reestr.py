1fb19ae10564b59458bbc2f46c17f165c4a8a938
Vladimir Korotkov <kva@delrus.ru>
2017-11-01 10:51:11 +0500

    Сделал скрипт append2reestr.py

--- bhpassport/append2reestr.py
+++ bhpassport/append2reestr.py
@@ -1,22 +1,67 @@
+# Выдергивает из TeX - файла с паспортом скважины параметры скважины и формирует строку для вставки
+# в XLS-файл с реестром скважин
+
 import re
-
-s = '\newcommand{\txtDepth}{12.0}  % Глубина скважины'
-pattern = re.compile('{(\d*.\d*)}')
-
-if re.findall(r'.*\newcommand{\txtDepth}', s):
-    Depth = pattern.findall(s)[0].replace('.', ',')
-    print('Depth: {}'.format(Depth))
-else:
-    print('Не найден')
-
-'''
-Clipboard
-
-To use native Python directories, use:
-
 import subprocess
+import os
 
+FILENAME = 'bhpassport.tex'             # Имя TeX - файла с паспортом скважины
+
+# s = '\newcommand{\txtDepth}{12.0}  % Глубина скважины'
+pattern1 = re.compile('{(\d*.\d*)}')     # Шаблон 1
+pattern2 = re.compile('.*{.*}{(.*)}')   # Шаблон 2
+
+
+# Копирует строку txt в буфер обмена (работает в Windows)
 def copy2clip(txt):
    cmd='echo '+txt.strip()+'|clip'
    return subprocess.check_call(cmd, shell=True)
-'''
+
+
+with open(FILENAME, encoding='utf-8') as file:
+    content = file.readlines()
+
+
+for line in content:
+    # print(line.strip())
+    # Depth
+    if re.search(r'.*\\newcommand{\\txtDepth}', line):
+        # print(line.strip())
+        Depth = pattern1.findall(line)[0].replace('.', ',')
+    # Debit
+    if re.search(r'.*\\newcommand{\\txtDebit}', line):
+        # print(line.strip())
+        Debit = pattern1.findall(line)[0].replace('.', ',')
+    # Coordinates
+    if re.search(r'.*\\newcommand{\\txtCoords}', line):
+        # print(line.strip())
+        Coordinates = pattern2.findall(line)[0]
+    # Сadaster
+    if re.search(r'.*\\newcommand{\\txtCadaster}', line):
+        # print(line.strip())
+        Cadaster = pattern2.findall(line)[0]
+    # Address
+    if re.search(r'.*\\newcommand{\\txtAddress}', line):
+        # print(line.strip())
+        Address = pattern2.findall(line)[0]
+
+print('Depth    : {}'.format(Depth))
+print('Debit    : {}'.format(Debit))
+print('Coords   : {}'.format(Coordinates))
+
+(lat, lon) = Coordinates.split()
+print('Lat      : {}'.format(lat))
+print('Lon      : {}'.format(lon))
+
+print('Cadaster : {}'.format(Cadaster))
+print('Address  : {}'.format(Address))
+
+full_path = '{}\{}'.format(os.getcwd(), FILENAME)
+
+res = '0\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}'.format(
+    'Date', Address, Address, Cadaster, lat, lon, 0, Depth, Debit, full_path)
+
+print(res)
+copy2clip(res)
+print('\n Результат скопирован в буфер обмена ')
+
